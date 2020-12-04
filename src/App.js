01e8c95f8b62e86693eeb97b250a242d6e7c1450
Gabriel Siqueira <gabrielsdo1222@gmail.com>
2020-12-04 14:52:24 -0300

    ListagemReact

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Routes from "./routes";
+
+import './styles.css';
+
+import Header from './Components/Header';
+
+
+const App = () => (
+
+  <div className="App">
+    <Header></Header>
+    <Routes/>
+ </div>
+
+);
+
+
+export default App;
---
+++ src/Components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header =  () => (
+    <header id="main-header">Listagem de Produtos em React</header>
+);
+
+export default Header;
